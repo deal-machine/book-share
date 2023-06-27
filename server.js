57f800febf687a85273f9f78bdd7a3adacd95931
deal-machine <95deal@gmail.com>
2023-06-27 17:48:41 -0300

    feat: include socket.io to real-time

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+import http from "http";
 import app from "./src/app.js";
+import { webSocket } from "./websocket.js";
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Express server running on port ${port}`));
+const server = http.createServer(app);
+
+server.listen(port, () =>
+  console.log(`Express server running on port ${port}`)
+);
+
+const io = webSocket(server);
+
+io.on("connection", (socket) => {
+  console.log("socket");
+});
