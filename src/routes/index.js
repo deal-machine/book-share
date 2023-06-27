0d11c8379e486a1013f73e2768ec67685210ce66
deal-machine <95deal@gmail.com>
2023-06-27 15:43:29 -0300

    refactor: remove unused routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,4 @@ const routes = Router();
 
 routes.use("/books", bookRouters);
 
-routes.get("/", (req, resp) => {
-  return resp.status(200).json({ message: "Main page" });
-});
-
-routes.get("/authors", (req, resp) => {
-  return resp.status(200).json({ message: "Author page" });
-});
-
 export default routes;
